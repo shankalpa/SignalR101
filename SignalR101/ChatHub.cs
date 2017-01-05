dc60539c97325f24057c46dbd4ecdcf38f0bcdf2
shankalpa <shan.kalp.x@gmail.com>
2017-01-05 11:31:51 -0600

    First Commit

---
+++ SignalR101/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR101 
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void BroadCastMessage(string messgaeFrom, string message)
+        {
+            Clients.All.receiveMessage(messgaeFrom, message);
+        }
+    }
+}
